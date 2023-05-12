6c137e24f07a69d59dfdfdd831ab37df622fc147
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-05-12 14:01:49 -0400

    Added image url resolver to automapper

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -11,7 +11,8 @@ namespace API.Helpers
         {
             CreateMap<Product, ProductReturnDTO>().
                 ForMember(dest => dest.ProductType, opt => opt.MapFrom(src => src.ProductType.Name)).
-                ForMember(dest => dest.ProductBrand, opt => opt.MapFrom(src => src.ProductBrand.Name));
+                ForMember(dest => dest.ProductBrand, opt => opt.MapFrom(src => src.ProductBrand.Name)).
+                ForMember(dest => dest.PictureUrl, opt => opt.MapFrom<ProductUrlResolver>());
         }
     }
 }
---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,25 @@
+﻿using API.DTOs;
+using AutoMapper;
+using Core.Models;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductReturnDTO, string>
+    {
+        private readonly IConfiguration _config;
+
+        public ProductUrlResolver(Microsoft.Extensions.Configuration.IConfiguration config) 
+        {
+            this._config = config;
+        }
+        public string Resolve(Product source, ProductReturnDTO destination, string destMember, ResolutionContext context)
+        {
+            if(!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _config["ApiUrl"] + source.PictureUrl;
+            }
+
+            return null;
+        }
+    }
+}

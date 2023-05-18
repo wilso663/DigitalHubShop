590c1530b70b1bee797329475289482fdd6c372d
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-05-18 15:56:18 -0400

    Added navbar component. Rendered API product subscription in app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'DigitalHubShop';
+  title = 'DigiShop';
+  products: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get('https://localhost:7068/api/products').subscribe({
+      next: (response: any) => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => {
+        console.log('request complete')
+      }
+    });
+  }
 }
+
+

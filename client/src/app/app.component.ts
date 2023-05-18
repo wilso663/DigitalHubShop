e35e4308bd932bf6b6d72be84becae80378f9d06
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-05-18 16:09:01 -0400

    Added interfaces for product and pagination data in UI client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,11 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DigiShop';
-  products: any[] = [];
+  products: Product[] = [];
 
   constructor(private http: HttpClient) {
-    this.http.get('https://localhost:7068/api/products').subscribe({
-      next: (response: any) => this.products = response.data,
+    this.http.get<Pagination<Product[]>>('https://localhost:7068/api/products').subscribe({
+      next: response => this.products = response.data,
       error: error => console.log(error),
       complete: () => {
         console.log('request complete')

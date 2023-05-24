d6a964831e3be2c913c7ab586be6f4c2fedeb25f
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-05-24 15:46:03 -0400

    Added section header title. Added breadcrumb service package, used in section header template.

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -6,6 +6,8 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 
 
@@ -15,6 +17,7 @@ import { ToastrModule } from 'ngx-toastr';
     TestErrorComponent,
     NotFoundComponent,
     ServerErrorComponent,
+    SectionHeaderComponent,
   ],
   imports: [
     CommonModule,
@@ -23,9 +26,11 @@ import { ToastrModule } from 'ngx-toastr';
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
     }),
+    BreadcrumbModule,
   ],
   exports: [
     NavBarComponent,
+    SectionHeaderComponent
   ]
 })
 export class CoreModule { }

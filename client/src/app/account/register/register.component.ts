b523e9073e67d22ed9309a1b5289746fbd10ad66
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-06-07 01:02:27 -0400

    Added async validation interceptor for register component for if email already exists

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { AccountService } from '../account.service';
-import { FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, AsyncValidator, AsyncValidatorFn, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { map, finalize, debounceTime, switchMap, take } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -21,7 +22,7 @@ export class RegisterComponent {
 
   registerForm = this.formBuilder.group({
     displayName: ['', Validators.required],
-    email: ['', [Validators.email, Validators.required]],
+    email: ['', [Validators.email, Validators.required], [this.validateEmailNotTaken()]],
     password: ['', [Validators.required, Validators.pattern(this.passwordRegex)]]
   });
 
@@ -30,5 +31,20 @@ export class RegisterComponent {
       next: () => this.router.navigateByUrl('/shop'),
       error: error => this.errors = error.errors
     })
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return (control: AbstractControl) => {
+      return control.valueChanges.pipe(
+        debounceTime(1000),
+        take(1),
+        switchMap(() => {
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map(result => result ? {emailExists: true} : null),
+            finalize(() => control.markAsTouched())
+          )
+        })
+      )
+    }
   }
 }

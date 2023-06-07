7b5ed9fa54b8a2a6288ee7ae608bd6311b9c8466
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-06-07 00:37:32 -0400

    Added register model state validation if email address is already taken

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  errors: string[] | null = null;
+
   constructor(
     private formBuilder: FormBuilder,
     private accountService: AccountService,
@@ -25,7 +27,8 @@ export class RegisterComponent {
 
   onSubmit() {
     this.accountService.register(this.registerForm.value).subscribe({
-      next: () => this.router.navigateByUrl('/shop')
+      next: () => this.router.navigateByUrl('/shop'),
+      error: error => this.errors = error.errors
     })
   }
 }

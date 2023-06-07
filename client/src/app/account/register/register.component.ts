49042cf255809a2254f7b5a155145f3323a1cd82
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-06-07 00:24:08 -0400

    Added password validation to form text input component, added register component template with feedback highlighting

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../account.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(
+    private formBuilder: FormBuilder,
+    private accountService: AccountService,
+    private router: Router
+  ) {}
 
+  passwordRegex = "(?=^.{6,10}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$"
+
+  registerForm = this.formBuilder.group({
+    displayName: ['', Validators.required],
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required, Validators.pattern(this.passwordRegex)]]
+  });
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigateByUrl('/shop')
+    })
+  }
 }

12eca079c9278bcf4007dc5ea72dbf52e9ee86cf
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-06-07 01:44:42 -0400

    Added auth guard to checkout. Switched account service observable to replay subject to cache tokens.

---
+++ client/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, map } from 'rxjs';
+import { AccountService } from 'src/app/account/account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+
+  constructor(private accountService: AccountService, private router: Router){}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | undefined> {
+    return this.accountService.currentUser$.pipe(
+      map(auth => {
+        if (auth) {
+          return true;
+        }
+        this.router.navigate(['account/login'], {queryParams: {returnUrl: state.url}});
+        return false;
+      })
+    );
+  }
+  
+}

bddde8b9f0ae09149351cb88e23fcad3e872dd8b
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-06-08 00:37:20 -0400

    Implemented checkout form payment template and module, delivery form addition to basket subtotal, and successful payment template.

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environment/environment';
 import { Basket, BasketItem, BasketTotals } from '../shared/models/basket';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../shared/models/product';
+import { DeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,15 @@ export class BasketService {
   basketSource$ = this.basketSource.asObservable();
   private basketTotalSource = new BehaviorSubject<BasketTotals | null>(null);
   basketTotalSource$ = this.basketTotalSource.asObservable();
+  shipping = 0;
 
   constructor(private http: HttpClient) { 
     
+  }
+
+  setShippingPrice(deliveryMethod: DeliveryMethod){
+    this.shipping = deliveryMethod.price;
+    this.calculateBasketTotals();
   }
 
   getBasket(id: string){
@@ -67,11 +74,15 @@ export class BasketService {
   deleteBasket(basket: Basket) {
     return this.http.delete(this.baseUrl + 'basket?id=' + basket.id).subscribe({
       next: () => {
-        this.basketSource.next(null);
-        this.basketTotalSource.next(null);
-        localStorage.removeItem("basket_id");
+        this.deleteLocalBasket();
       }
     });
+  }
+
+  deleteLocalBasket(){
+    this.basketSource.next(null);
+    this.basketTotalSource.next(null);
+    localStorage.removeItem("basket_id");
   }
 
   private addOrUpdateItem(items: BasketItem[], itemToAdd: BasketItem, quantity: number): BasketItem[] {
@@ -105,10 +116,9 @@ export class BasketService {
     const basket = this.getCurrentBasketValue();
     if(!basket) return;
 
-    const shipping = 0;
     const subtotal = basket.items.reduce((sum, curr) => (curr.price * curr.quantity) + sum, 0);
-    const total = subtotal + shipping;
-    this.basketTotalSource.next({shipping,total,subtotal});
+    const total = subtotal + this.shipping;
+    this.basketTotalSource.next({shipping: this.shipping, total,subtotal});
   }
 
   private isProduct(item: Product | BasketItem): item is Product {
--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environment/environment';
 import { DeliveryMethod } from '../shared/models/deliveryMethod';
 import { map } from 'rxjs';
+import { Order, OrderToCreate } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,10 @@ export class CheckoutService {
   baseUrl = environment.apiUrl;
 
   constructor(private httpClient: HttpClient) { }
+
+  createOrder(order: OrderToCreate){
+    return this.httpClient.post<Order>(this.baseUrl + 'orders', order);
+  }
 
   getDeliveryMethods() {
     return this.httpClient.get<DeliveryMethod[]>(this.baseUrl + 'orders/deliveryMethods').pipe(

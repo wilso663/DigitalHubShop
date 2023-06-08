c82a671f9d161d39604ee0d96a0d112e5e062d0d
Stephen Wilson <Stephen.Ben.Wilson@gmail.com>
2023-06-08 16:21:03 -0400

    Added stripe elements on checkout payment component and template

--- client/src/app/checkout/checkout-payment/checkout-payment.component.ts
+++ client/src/app/checkout/checkout-payment/checkout-payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { BasketService } from 'src/app/basket/basket.service';
 import { CheckoutService } from '../checkout.service';
@@ -6,34 +6,60 @@ import { ToastrService } from 'ngx-toastr';
 import { Basket } from 'src/app/shared/models/basket';
 import { Address } from 'src/app/shared/models/user';
 import { NavigationExtras, Router } from '@angular/router';
+import { Stripe, StripeCardCvcElement, StripeCardExpiryElement, StripeCardNumberElement, loadStripe } from '@stripe/stripe-js';
 
 @Component({
   selector: 'app-checkout-payment',
   templateUrl: './checkout-payment.component.html',
   styleUrls: ['./checkout-payment.component.scss']
 })
-export class CheckoutPaymentComponent {
+export class CheckoutPaymentComponent implements OnInit {
   @Input() checkoutForm?: FormGroup;
+  @ViewChild('cardNumber') cardNumberElement?: ElementRef;
+  @ViewChild('cardExpiry') cardExpiryElement?: ElementRef;
+  @ViewChild('cardCvc') cardCvcElement?: ElementRef;
+  stripe: Stripe | null = null;
+  cardNumber?: StripeCardNumberElement;
+  cardExpiry?: StripeCardExpiryElement;
+  cardCvc?: StripeCardCvcElement;
 
+  
   constructor(private basketService: BasketService, 
     private checkoutService: CheckoutService, 
     private toastr: ToastrService,
     private router: Router){}
 
-    submitOrder() {
-      const basket = this.basketService.getCurrentBasketValue();
-      if(!basket) return;
-      const orderToCreate = this.getOrderToCreate(basket);
-      if(!orderToCreate) return;
-      this.checkoutService.createOrder(orderToCreate).subscribe({
-        next: order => {
-          this.toastr.success('Order created successfully')
-          this.basketService.deleteLocalBasket();
-          const navigationExtras: NavigationExtras = {state: order};
-          this.router.navigate(['checkout/success'], navigationExtras);
-        }
-      })
-    }
+  ngOnInit(): void {
+    loadStripe("pk_test_51NGnI3CiMNFMiLFE27qdOWkN90rI7np2MjkqbXTqvObsTtWSJh2Gtnwl1agD4Rk9JCRrfCGkP8cqZsTWs2wyfXMl00kNRdaatw").then(stripe => {
+      this.stripe = stripe;
+      const elements = stripe?.elements();
+      if(elements){
+        this.cardNumber = elements.create('cardNumber');
+        this.cardNumber.mount(this.cardNumberElement?.nativeElement);
+
+        this.cardExpiry = elements.create('cardExpiry');
+        this.cardExpiry.mount(this.cardExpiryElement?.nativeElement);
+
+        this.cardCvc = elements.create('cardCvc');
+        this.cardCvc.mount(this.cardCvcElement?.nativeElement);
+      }
+    })
+  }
+
+  submitOrder() {
+    const basket = this.basketService.getCurrentBasketValue();
+    if(!basket) return;
+    const orderToCreate = this.getOrderToCreate(basket);
+    if(!orderToCreate) return;
+    this.checkoutService.createOrder(orderToCreate).subscribe({
+      next: order => {
+        this.toastr.success('Order created successfully')
+        this.basketService.deleteLocalBasket();
+        const navigationExtras: NavigationExtras = {state: order};
+        this.router.navigate(['checkout/success'], navigationExtras);
+      }
+    })
+  }
   
   private getOrderToCreate(basket: Basket) {
     const deliveryMethodId = this.checkoutForm?.get('deliveryForm')?.get('deliveryMethod')?.value;
